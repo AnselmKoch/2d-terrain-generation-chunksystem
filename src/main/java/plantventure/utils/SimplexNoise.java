61b53b334bb0b86da9a442e7da975f1204004efe
Anselm Koch <anselmo55343@gmail.com>
2023-03-07 19:27:50 +0100

    Add author of SimplexNoise.java

--- src/main/java/plantventure/utils/SimplexNoise.java
+++ src/main/java/plantventure/utils/SimplexNoise.java
@@ -1,5 +1,22 @@
 package plantventure.utils;
 
+/*
+ * A speed-improved simplex noise algorithm for 2D, 3D and 4D in Java.
+ *
+ * Based on example code by Stefan Gustavson (stegu@itn.liu.se).
+ * Optimisations by Peter Eastman (peastman@drizzle.stanford.edu).
+ * Better rank ordering method by Stefan Gustavson in 2012.
+ *
+ * This could be speeded up even further, but it's useful as it is.
+ *
+ * Version 2012-03-09
+ *
+ * This code was placed in the public domain by its original author,
+ * Stefan Gustavson. You may use it as you see fit, but
+ * attribution is appreciated.
+ *
+ */
+
 public class SimplexNoise {  // Simplex noise in 2D, 3D and 4D
     private static Grad grad3[] = {new Grad(1,1,0),new Grad(-1,1,0),new Grad(1,-1,0),new Grad(-1,-1,0),
             new Grad(1,0,1),new Grad(-1,0,1),new Grad(1,0,-1),new Grad(-1,0,-1),
